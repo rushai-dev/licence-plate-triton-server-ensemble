104450512f25d621b2f092707b10304b62043aa5
RUSH.AI <ratchaphonboss@gmail.com>
2023-04-17 19:34:58 +0700

    remove comment

--- model_repository/preprocess/1/model.py
+++ model_repository/preprocess/1/model.py
@@ -2,11 +2,6 @@ import numpy as np
 import sys
 import json
 import io
-
-# triton_python_backend_utils is available in every Triton Python model. You
-# need to use this module to create inference requests and responses. It also
-# contains some utility functions for extracting information from model_config
-# and converting Triton input/output types to numpy types.
 import triton_python_backend_utils as pb_utils
 
 from PIL import Image
@@ -15,25 +10,8 @@ import os
 
 
 class TritonPythonModel:
-    """Your Python model must use the same class name. Every Python model
-    that is created must have "TritonPythonModel" as the class name.
-    """
 
     def initialize(self, args):
-        """`initialize` is called only once when the model is being loaded.
-        Implementing `initialize` function is optional. This function allows
-        the model to intialize any state associated with this model.
-        Parameters
-        ----------
-        args : dict
-          Both keys and values are strings. The dictionary keys and values are:
-          * model_config: A JSON string containing the model configuration
-          * model_instance_kind: A string containing model instance kind
-          * model_instance_device_id: A string containing model instance device ID
-          * model_repository: Model repository path
-          * model_version: Model version
-          * model_name: Model name
-        """
 
         # You must parse model_config. JSON string is not parsed here
         self.model_config = model_config = json.loads(args['model_config'])
@@ -47,33 +25,13 @@ class TritonPythonModel:
             output0_config['data_type'])
 
     def execute(self, requests):
-        """`execute` MUST be implemented in every Python model. `execute`
-        function receives a list of pb_utils.InferenceRequest as the only
-        argument. This function is called when an inference request is made
-        for this model. Depending on the batching configuration (e.g. Dynamic
-        Batching) used, `requests` may contain multiple requests. Every
-        Python model, must create one pb_utils.InferenceResponse for every
-        pb_utils.InferenceRequest in `requests`. If there is an error, you can
-        set the error argument when creating a pb_utils.InferenceResponse
-        Parameters
-        ----------
-        requests : list
-          A list of pb_utils.InferenceRequest
-        Returns
-        -------
-        list
-          A list of pb_utils.InferenceResponse. The length of this list must
-          be the same as `requests`
-        """
 
         output0_dtype = self.output0_dtype
 
         responses = []
 
-        # Every Python backend must iterate over everyone of the requests
-        # and create a pb_utils.InferenceResponse for each of them.
         for request in requests:
-            # Get INPUT0
+
             in_0 = pb_utils.get_input_tensor_by_name(request, "INPUT_0")
 
             normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
@@ -86,7 +44,6 @@ class TritonPythonModel:
 
             def image_loader(image_name):
                 image = loader(image_name)
-                #expand the dimension to nchw
                 image = image.unsqueeze(0)
                 return image
 
@@ -99,24 +56,11 @@ class TritonPythonModel:
             out_tensor_0 = pb_utils.Tensor("OUTPUT_0",
                                            img_out.astype(output0_dtype))
 
-            # Create InferenceResponse. You can set an error here in case
-            # there was a problem with handling this inference request.
-            # Below is an example of how you can set errors in inference
-            # response:
-            #
-            # pb_utils.InferenceResponse(
-            #    output_tensors=..., TritonError("An error occured"))
             inference_response = pb_utils.InferenceResponse(
                 output_tensors=[out_tensor_0])
             responses.append(inference_response)
 
-        # You should return a list of pb_utils.InferenceResponse. Length
-        # of this list must match the length of `requests` list.
         return responses
 
     def finalize(self):
-        """`finalize` is called only once when the model is being unloaded.
-        Implementing `finalize` function is OPTIONAL. This function allows
-        the model to perform any necessary clean ups before exit.
-        """
         print('Cleaning up...')
